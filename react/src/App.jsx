b20e013c0bf977f0a5ac3b707a9bea2257e685c6
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-10-10 23:29:49 +0530

    added profile card component

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -11,20 +11,26 @@ function App() {
       }}
     >
       <div style={{ display: "flex" }}>
+        <div id="card-profile" style={{ marginRight: 100, marginTop: 50 }}>
+          <ProfileCard />
+        </div>
         <div
           style={{
             display: "flex",
             flexDirection: "column",
+            marginRight: 500,
           }}
         >
-          <div>
-            <PostComponent />
-          </div>
-          <div>
-            <PostComponent />
-          </div>
-          <div>
-            <PostComponent />
+          <div id="all-posts" style={{ width: 600, marginTop: 50 }}>
+            <div>
+              <PostComponent />
+            </div>
+            <div>
+              <PostComponent />
+            </div>
+            <div>
+              <PostComponent />
+            </div>
           </div>
         </div>
       </div>
@@ -33,13 +39,13 @@ function App() {
 }
 
 const style = {
-  width: 400,
+  width: 600,
   backgroundColor: "white",
   borderRadius: 10,
   borderColor: "grey",
   borderWidth: 1,
   padding: 20,
-  marginBottom: 20,
+  marginBottom: 40,
 };
 
 function PostComponent() {
@@ -64,6 +70,74 @@ function PostComponent() {
       <div style={{ fontSize: 12 }}>
         Want to know how to win big? Check out how this folks won $6000 of
         bounty
+      </div>
+    </div>
+  );
+}
+
+function ProfileCard() {
+  return (
+    <div id="profile-card">
+      <div
+        style={{
+          height: 400,
+          width: 400,
+          marginBottom: 20,
+          backgroundColor: "white",
+          borderRadius: 50,
+        }}
+      >
+        <img
+          src="https://media.licdn.com/dms/image/v2/D5603AQF2JM6Dnd3uCQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1727767853473?e=1733961600&v=beta&t=BO80R-WXtjqNQAkwhJb1PJAPKt8EthuuU4-YGw-YSdc"
+          alt="your profile pic"
+          style={{
+            width: 90,
+            height: 90,
+            borderRadius: 50,
+            marginTop: 50,
+            marginLeft: 40,
+          }}
+        ></img>
+        <div>
+          <h1
+            style={{
+              color: "black",
+              marginTop: 20,
+              marginLeft: 30,
+              marginRight: 30,
+            }}
+          >
+            Subhajit Chaudhury
+          </h1>
+          <p style={{ color: "black", marginLeft: 30 }}>
+            🚀Tech Geek | B.Tech Student | Exploring MERN Stack & TypeScript |
+            Open-Source Contributor🌟
+          </p>
+          <div style={{ color: "black", marginLeft: 30 }}>
+            Jamshedpur , Jharkhand
+          </div>
+          <div
+            style={{
+              border: "2px dotted black",
+              height: 50,
+              width: 350,
+              marginLeft: 20,
+              marginTop: 20,
+              borderRadius: 10,
+            }}
+          >
+            <p
+              style={{
+                color: "black",
+                fontSize: 25,
+                marginLeft: 100,
+                marginTop: 10,
+              }}
+            >
+              + Experience
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
