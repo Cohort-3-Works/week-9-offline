bd1fb66cf2a851ae34be49ff313b1fe169625398
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-10-11 23:19:24 +0530

    created add post button and learnt how to iterate over array and make it a component

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,6 +4,35 @@ import { PostComponent } from "./PostComponent";
 import { ProfileCard } from "./ProfileCard";
 
 function App() {
+  const posts = [
+    {
+      title: "Subhajit Chaudhury",
+      followerCount: 23888,
+      time: "12m",
+      image:
+        "https://media.licdn.com/dms/image/v2/D5603AQF2JM6Dnd3uCQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1727767853473?e=1733961600&v=beta&t=BO80R-WXtjqNQAkwhJb1PJAPKt8EthuuU4-YGw-YSdc",
+      description:
+        "Want to know how to win big? Check out how this folks won $6000 of bounty",
+    },
+  ];
+
+  // based on this posts array i want to render the components thats why somehow we need to use for loop here
+  // how can we iterate over it ?
+
+  // i want to render more and more post for that i need to create an array of posts
+
+  const postComponents = posts.map((post) => (
+    <PostComponent
+      title={post.title}
+      followerCount={post.followerCount}
+      time={post.time}
+      image={post.image}
+      description={post.description}
+    />
+  ));
+
+  function addPost() {}
+
   return (
     <div
       style={{
@@ -16,6 +45,7 @@ function App() {
       <div style={{ display: "flex" }}>
         <div id="card-profile" style={{ marginRight: 100, marginTop: 50 }}>
           <ToggleButton />
+          <button onClick={addPost}>Add Post</button>
           <ProfileCard />
         </div>
         <div
@@ -26,15 +56,7 @@ function App() {
           }}
         >
           <div id="all-posts" style={{ width: 600, marginTop: 50 }}>
-            <div>
-              <PostComponent
-                userName="Subhajit Chaudhury"
-                followerCount={23888}
-                time="12m"
-                image="https://media.licdn.com/dms/image/v2/D5603AQF2JM6Dnd3uCQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1727767853473?e=1733961600&v=beta&t=BO80R-WXtjqNQAkwhJb1PJAPKt8EthuuU4-YGw-YSdc"
-                description="Want to know how to win big? Check out how this folks won $6000 of bounty"
-              />
-            </div>
+            <div>{postComponents}</div>
           </div>
         </div>
       </div>
